ad8b74d481c9d0583266c00822ece68d65bdcb9c
NickTheWinter <k.zimenkov@mail.ru>
2022-09-20 00:01:33 +0300

    add reading from file

--- Preparing/Program.cs
+++ Preparing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,19 @@ namespace Preparing
     {
         static void Main(string[] args)
         {
+            string path = @".\enterInfo.txt";
+            StreamReader streamReader = new StreamReader(path);
+            string line = streamReader.ReadLine();
+            List<string[]> strings = new List<string[]>(); 
+            while (line != null && line != "")
+            {
+                //Сначала ввод длины, внешний диаметр, толщина стенки
+                string[] array = line.Split(';');
+                strings.Add(array);
+                line = streamReader.ReadLine();
+            }
             
-
+            Console.ReadLine();
         }
     }
 }
